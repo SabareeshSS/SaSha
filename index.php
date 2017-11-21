b181a0035f8a2deba0cfdc7298fc2aad38623d44
Sabareesh <SabareeshSS@users.noreply.github.com>
2017-11-21 12:08:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <?php include_once("home.html") ?>
+Hello Team,
+
+With respect to my Insurance certificate number : OG-17-1000-6609-00504101, 
+
+I have made a purchase of Sony television Model Number : KLV 32W512D IN5 18769001 on 17th of September 2016 which covers a Standard Warranty of One year only whereas in my policy it has been updated as 2 years and my CPP Asset Care Warranty starts only from 17th of Sepetember 2018 instead 17th of Sepetember 2017.
+
+I fear if something happens to my product this year I have no insurance covered so I kindly request you to update my policy details accordingly as,
+
+Ext. Warranty Start Date : 17-Sept-2017
+Ext. Warranty End Date   : 16-Sept-2020
+
+
+For your reference, I have attached the Warranty Card attested by the Authorized dealer of Sony with this mail.
+
+Many Thanks,
+Sabareesh SS
+(Membership Number:AC0028976) 
